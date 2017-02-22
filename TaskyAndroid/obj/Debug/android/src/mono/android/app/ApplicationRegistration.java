c627bd54c1025e098a613af60e549839e055c351
Jose F Martinez <jose.f.martinez-pedraza@nasa.gov>
2017-02-21 20:25:46 -0400

    Changing project to Tasky, which contains apps for android and ios, plus the portable code.

---
+++ TaskyAndroid/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("TaskyAndroid.TaskyApp, TaskyAndroid, Version=1.0.6242.24474, Culture=neutral, PublicKeyToken=null", md52562d6e10a3afb80be0d467891322c3b.TaskyApp.class, md52562d6e10a3afb80be0d467891322c3b.TaskyApp.__md_methods);
+		
+	}
+}
